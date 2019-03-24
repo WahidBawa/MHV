ae89ff60ff31edf4192dece2d81504dd0002ae97
Wahid Bawa <wahidbawa10@gmail.com>
2019-03-24 06:44:45 -0400

    rotation is working now

--- Catch These Hands/Assets/CubeMove.cs
+++ Catch These Hands/Assets/CubeMove.cs
@@ -14,16 +14,31 @@ public class CubeMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        string s = "";
         GameObject leftHand = GameObject.Find("RigidRoundHand_L");
+        GameObject rightHand = GameObject.Find("RigidRoundHand_R");
+
         if (leftHand != null)
         {
             DataFetcher data = leftHand.GetComponent<DataFetcher>();
             this.transform.Translate((float)-data.roll / 800, 0, (float) data.pitch / 800);
-            System.IO.File.WriteAllText("../Light_Havoc/tmp.tmp", -data.roll + "," + data.pitch);
+            s += -data.roll + "," + data.pitch;
         } else
         {
-            System.IO.File.WriteAllText("../Light_Havoc/tmp.tmp", "0,0");
+            s +="0,0";
         }
+        s += " ";
+        if (rightHand != null)
+        {
+            Rotation data = rightHand.GetComponent<Rotation>();
+            this.transform.Rotate(0, (float)(data.angle), 0);
+            s += data.angle;
+        }
+        else
+        {
+            s += "0";
+        }
+        System.IO.File.WriteAllText("../Light_Havoc/tmp.tmp", s);
 
     }
 }
