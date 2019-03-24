f8d04df1387066d61f37b4b3900b49c505ce0fdc
Wahid Bawa <wahidbawa10@gmail.com>
2019-03-24 12:04:36 -0400

    fixed error

--- Light_Havoc/Shotgun.java
+++ Light_Havoc/Shotgun.java
@@ -7,13 +7,14 @@ public class Shotgun extends Weapon{
 	private final int[] DAMAGE = new int[] {1, 2, 3, 5, 7};
 	private int damage;
 	private Random rand = new Random();
-
+	private int amountOfBullets;
 	public Shotgun (int tin) {
 
 		tier = tin;
 		type = "Shotgun";
 		damage = DAMAGE[tier];
 		image = null;
+		amountOfBullets = tier + 3;
 		try {
 			 image = ImageIO.read(new File("weapons/weapon00.png"));
 		} catch (IOException e) {System.out.println("Image not found");}
@@ -21,8 +22,8 @@ public class Shotgun extends Weapon{
 	}
 
 	public Projectile[] use(double ang, double x, double y) {
-		Projectile[] projectiles = new Projectile[tier + 3];
-		for (int i = 0; i < tier + 3; i++) projectiles[i] = new Projectile(x, y, 8, (ang - Math.toRadians(40)) + Math.toRadians(rand.nextInt(80)), "bullet", damage);
+		Projectile[] projectiles = new Projectile[amountOfBullets];
+		for (int i = 0; i < amountOfBullets; i++) projectiles[i] = new Projectile(x, y, 8, (ang - Math.toRadians(40)) + Math.toRadians(rand.nextInt(80)), "bullet", damage);
 		return projectiles;
 	}
 }
