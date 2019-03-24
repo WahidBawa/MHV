908d020a45383d942a09b22251fbb3f07fedc097
Wahid Bawa <wahidbawa10@gmail.com>
2019-03-24 08:19:17 -0400

    merge

---
+++ Catch These Hands/Assets/PewPew.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PewPew : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public double dist;
+    public Vector3 rightHand;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rightHand = this.transform.Find("palm").position;
+        dist = (this.transform.Find("index").Find("bone3").position.z - this.transform.Find("palm").position.z) * 100;
+
+    }
+}
--- Catch These Hands/Assets/Rotation.cs
+++ Catch These Hands/Assets/Rotation.cs
@@ -6,6 +6,9 @@ public class Rotation : MonoBehaviour
 {
     Vector3 palm;
     public double angle;
+    public double dist;
+    public double pewing = 0;
+    //public Vector3 rightHand;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,12 +25,16 @@ public class Rotation : MonoBehaviour
             float dx = (leftHand.transform.Find("palm").position.x + rightHand.x) / 2.0f;
             float dz = (leftHand.transform.Find("palm").position.z + rightHand.z) / 2.0f;
             angle = (Mathf.Rad2Deg*(Mathf.Atan2(dz, dx)) - 90) / 20;
-            Debug.Log("X: " + dx + " Z: " + dz + " ANGLE: " + angle);
+            //Debug.Log("X: " + dx + " Z: " + dz + " ANGLE: " + angle);
 
         }
         else
         {
             angle = 0;
         }
+        rightHand = this.transform.Find("palm").position;
+        dist = (this.transform.Find("index").Find("bone3").position.z - this.transform.Find("palm").position.z) * 100;
+        pewing = 0;
+        if (dist < 2) pewing = 1;
     }
 }
