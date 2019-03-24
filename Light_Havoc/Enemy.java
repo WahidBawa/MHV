2f4a04aff47388203bc8cc516f9f4edc2c07d7e9
Jagraj Aulakh <aulakhjagraj@outlook.com>
2019-03-24 07:21:20 -0400

    Enemy stuff

--- Light_Havoc/Enemy.java
+++ Light_Havoc/Enemy.java
@@ -1,7 +1,16 @@
 public class Enemy extends Character{
 
+	private double angle;
+
 	public Enemy(double xin, double yin) {
 		x = xin;
 		y = yin;
+	}
+
+	public void update(Player player) {
+		double dx = player.getX() - x;
+		double dy = player.getY() - y;
+		angle = Math.min(1, Math.atan2(dy, dx));
+		move(2*Math.cos(angle), 2*Math.sin(angle));
 	}
 }
